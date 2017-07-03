be884b0c118dbd16a245fa8f8aeff231db95d3a0
8CF7K3FIUDL2BGT\Administrator <shohokuooo@gmail.com>
2017-07-03 15:49:34 +0800

    Finish getting start.

--- src/main/hello.py
+++ src/main/hello.py
@@ -1,47 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 This is the start of TensorFlow.
 
 How to creat this Pycharm project is more important than the following codes. If you need see the README.
-'''
+"""
 
 __author__ = 'Paul Yang'
-
 
 import tensorflow as tf
 
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 
-print(sess.run(hello))
-
+print("sess.run(hello): ", sess.run(hello))
 
 ## The Computational Graph
 # A computational graph is a series of TensorFlow operations arranged into a graph of nodes.
 node1 = tf.constant(3.0, dtype=tf.float32)
-node2 = tf.constant(4.0) # also tf.float32 implicitly
+node2 = tf.constant(4.0)  # also tf.float32 implicitly
 
 print(node1, node2)
-print(sess.run([node1, node2]))
+print("sess.run([node1, node2]): ", sess.run([node1, node2]))
 
-node3 = tf.add(node1, node2) # Operations are also nodes.
+node3 = tf.add(node1, node2)  # Operations are also nodes.
 print(node3)
 print("sess.run(node3): ", sess.run(node3))
-
 
 # A graph can be parameterized to accept external inputs, known as placeholders.
 a = tf.placeholder(tf.float32)
 b = tf.placeholder(tf.float32)
-adder_node = a + b # + provides a shortcut for tf.add(a, b)
-print(sess.run(adder_node, {a: 3, b: 4.5}))
-print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+adder_node = a + b  # + provides a shortcut for tf.add(a, b)
+print("run(adder_node, {a: 3, b: 4.5}): ", sess.run(adder_node, {a: 3, b: 4.5}))
+print("run(adder_node, {a: [1, 3], b: [2, 4]}): ", sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
 
 # We can make the computational graph more complex by adding another operation.
 add_and_triple = adder_node * 3.
-print(sess.run(add_and_triple, {a: 3, b: 4.5}))
-
+print("run(add_and_triple, {a: 3, b: 4.5}): ", sess.run(add_and_triple, {a: 3, b: 4.5}))
 
 # In machine learning we will typically want a model that can take arbitrary inputs
 W = tf.Variable([.3], dtype=tf.float32)
@@ -55,10 +51,10 @@ By contrast, variables are not initialized when you call tf.Variable.
 To initialize all the variables in a TensorFlow program, you must explicitly call a special operation as follows:
 '''
 init = tf.global_variables_initializer()
-sess.run(init) # If don't run this, tensorflow.python.framework.errors_impl.FailedPreconditionError: Attempting to use uninitialized value Variable_1
+sess.run(
+    init)  # If don't run this, tensorflow.python.framework.errors_impl.FailedPreconditionError: Attempting to use uninitialized value Variable_1
 
-print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
-
+print("run(linear_model, {x: [1, 2, 3, 4]}): ", sess.run(linear_model, {x: [1, 2, 3, 4]}))
 
 '''
 A loss function measures how far apart the current model is from the provided data.
@@ -70,7 +66,7 @@ Then, we sum all the squared errors to create a single scalar that abstracts the
 y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
-print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+print("run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}): ", sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 
 '''
  A variable is initialized to the value provided to tf.Variable but can be changed using operations like tf.assign
@@ -79,4 +75,18 @@ print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 fixW = tf.assign(W, [-1.])
 fixb = tf.assign(b, [1.])
 sess.run([fixW, fixb])
-print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+print("run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}): ", sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+# tf.train API
+'''
+TensorFlow can automatically produce derivatives given only a description of the model using the function tf.gradients.
+For simplicity, optimizers typically do this for you.
+'''
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+sess.run(init)  # reset values to incorrect defaults.
+for i in range(1000):
+    sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+
+print("run([W, b]): ", sess.run([W, b]))
